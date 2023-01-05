edbfd44867357abe228a36e346950b555507fe19
Youssef Khemakhem <khemakhemyoussef700@gmail.com>
2023-01-05 18:50:58 +0100

    sr

--- src/main/java/com/stpr/gestionstockprod/Repository/CategorieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.stpr.gestionstockprod.Repository;
-
-import com.stpr.gestionstockprod.Model.Categorie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CategorieRepository extends JpaRepository<Categorie, Long> {
-
-}
